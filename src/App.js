12801af28459e40685198f06ed4808275cc8601a
Fernando Salas <luisfernandosalasg@gmail.com>
2022-04-29 10:46:21 -0500

    Add Breaking Bad App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Quote from "./components/Quote";
+import Spinner from "./components/Spinner";
+
+const initialQuote = {
+	text: "Text",
+	author: "Author",
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [quote, setQuote] = useState(initialQuote);
+	const [spinner, setSpinner] = useState(true);
+
+	const callBReakingBadAPI = async () => {
+		setSpinner(true);
+		const url = "https://www.breakingbadapi.com/api/quote/random";
+		const resp = await fetch(url);
+		const [newQuote] = await resp.json();
+		const { quote, author } = newQuote;
+		setQuote({
+			text: quote,
+			author: author,
+		});
+		setSpinner(false);
+	};
+
+	useEffect(() => {
+		callBReakingBadAPI();
+	}, []);
+
+	return (
+		<div className="app">
+			<img
+				src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+				alt="logo"
+			/>
+			<button onClick={() => callBReakingBadAPI()}>Get another quote</button>
+			{spinner ? <Spinner /> : <Quote quote={quote} />}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Spinner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => {
+	return (
+		<div class="lds-default">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+};
+
+export default Spinner;
